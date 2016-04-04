c4d12feca69df894680033d86e35ac2495432e05
sonip396 <jorge.espinosacam@gmail.com>
2016-04-04 18:04:49 +0200

    Ejercicio 1 hecho

--- practica07.rkt
+++ practica07.rkt
@@ -2,17 +2,32 @@
 #lang r6rs
 (import (rnrs)
         (schemeunit))
+(define (dato-tree arbol) 
+    (car arbol))
 
+(define (hijos-tree arbol) 
+    (cdr arbol))
+
+(define (hoja-tree? arbol) 
+   (null? (hijos-tree arbol)))
                           ;-----------------;
-                          ;;;;Ejercicio 1;;;;
+                          ;;;;Ejercicio 1a;;;;
                           ;-----------------;
 ;;Definición de las funciones
 (define (tostring-tree tree)
-  #t)
+  (string-append (symbol->string (dato-tree tree))
+                 (tostring-bosque (hijos-tree tree))))
+(define (tostring-bosque bosque)
+  (if (null? bosque)
+      ""
+      (string-append (tostring-tree (car bosque)) (tostring-bosque (cdr bosque)))))
 (define tree '(a (b (c (d)) (e)) (f)))
+(define tree2 '(hola (que) tal (((estas)))))
 ;;Ejemplos del enunciado
-(display "\n\n               ________________Ejercicio 1________________ \n")
+(display "\n\n               ________________Ejercicio 1a________________ \n")
 (display "El resultado de concatenar los elementos del arbol (a (b (c (d)) (e)) (f)) es: ")
 (display (tostring-tree tree))
 ;;Pruebas
 (check-equal? (tostring-tree tree) "abcdef")
+(check-equal? (tostring-tree '(hola (que) (tal))) "holaquetal")
+(check-equal? (tostring-tree '(arbol (que (esta) (en)) (orden))) "arbolqueestaenorden")
